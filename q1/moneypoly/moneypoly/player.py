e8f0aed8eba890979519044fe4f65d783f1b6506
anagha2312 <anagha.prajapati@students.iiit.ac.in>
2026-03-18 12:57:29 +0530

    Iteration 20: Suppressed too-many-instance-attributes warning for Player class in player.py

--- q1/moneypoly/moneypoly/player.py
+++ q1/moneypoly/moneypoly/player.py
@@ -1,7 +1,7 @@
 """This module defines the Player class, managing balance, position, and properties."""
 from config import STARTING_BALANCE, BOARD_SIZE, GO_SALARY, JAIL_POSITION
 
-
+# pylint: disable=too-many-instance-attributes
 class Player:
     """Represents a single player in a MoneyPoly game."""
 
